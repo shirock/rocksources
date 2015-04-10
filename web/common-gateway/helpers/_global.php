6371a0ecadedbfc2c7385208adb189540b602739
rock <rock@rock>
2015-04-10 15:26:36 +0800

    Import common-gateway from google-code-repo. TODO follow fig standards PSR.

---
+++ web/common-gateway/helpers/_global.php
@@ -0,0 +1,13 @@
+<?php
+// This global helper will be applied to all applications.
+// You may only declare functions in a helper.
+// To put conditional declarations in this global helper is a good idea.
+/*
+if (! function_exists('bar')) {
+    function bar()
+    {
+        // function body
+    }
+}
+*/
+?>
