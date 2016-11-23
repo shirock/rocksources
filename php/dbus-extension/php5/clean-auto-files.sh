15a6d263043ff1ee87293bbbd37a7916b7963e7c
rock <shirock.tw@gmail.com>
2016-11-23 15:19:16 +0800

    按我評估結果並參考他人建議，將 php7 支援項目分開維護。 https://github.com/beberlei/whitewashing.de/blob/master/drafts/porting_extension_to_php7.rst

---
+++ php/dbus-extension/php5/clean-auto-files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm -f dbus.la dbus.lo introspect.lo libtool
+rm -f .deps Makefile.global acinclude.m4 aclocal.m4
+rm -f config.guess config.h config.h.in config.log config.nice config.sub config.status configure configure.in
+rm -f install-sh ltmain.sh missing mkinstalldirs run-tests.php Makefile Makefile.fragments Makefile.objects
+rm -rf .libs autom4te.cache build include modules
