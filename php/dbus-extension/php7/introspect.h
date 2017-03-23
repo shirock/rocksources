71ed8aa7b71255c7ca90dc4e2b42a18029d50ef7
rock <shirock.tw@gmail.com>
2017-03-23 17:55:55 +0800

    取消 dbus extension 更新至 php7 的工作。 php7 大幅改變內部的 class/object 管理機制，而 dbus extension 有太多自定型態，增加更新難度。 我放棄完整的更新工作。

--- php/dbus-extension/php7/introspect.h
+++
@@ -1,3 +0,0 @@
-xmlNode *php_dbus_find_interface_node(xmlDocPtr doc, char *interface);
-xmlNode *php_dbus_find_method_node(xmlNode *root, char *method);
-xmlNode **php_dbus_get_next_sig(xmlNode **it, char **sig);
