f1a4a02263aca698068e41c4b6f31ecbf7203326
rock <shirock@seed.net.tw>
2019-03-22 09:50:16 +0800

    Fix code. Add MessageModal.title(). More usage descriptions.

--- web/ui-components/src/modals.js
+++ web/ui-components/src/modals.js
@@ -6,10 +6,27 @@
 /**
 Spinner 待處理中的轉圈動畫
 
+方法
+----
+
 + Spinner.show(msg) 在畫面中央顯示一個代表處理中的轉圈圖示。
   它不和使用者互動，使用者不能自己關掉。
   必須呼叫 Spinner.hide() 隱藏。
 + Spinner.hide()
+
+自訂外觀
+--------
+
+Spinner 會自行建立一個預設的 modal 外觀。但使用者也可以自訂。
+
+參考 https://getbootstrap.com/docs/4.0/components/modal/ 自訂 Spinner 外觀。
+
++ Spinner 要求一個 id 為 "rock-spinner-modal" 的 Bootstrap modal 。
++ 若 modal 中包含一個 id 為 "rock-spinner-modal-body" 的區塊，將用於顯示 show() 方法的 msg 內容。
++ 建議不要加上 fade 效果。
+  如果呼叫 hide() 時還沒有跑完 fade 的視覺效果，你將會發現 hide() 沒有作用。
++ 建議不要加上關閉控制項(具 data-dismiss 屬性的元素)。
+  以免使用者在你的工作跑完之前就關掉 Spinner 效果。
 
 Required: Bootstrap 4.3 or later
  */
@@ -44,9 +61,10 @@ const Spinner = (function(){
 
   function show(msg) {
     __init__();
-    let msg_div = document.getElementById('rock-spinner-modal-body');
-    msg_div.innerHTML = (msg ? msg : '');
-    self.modal('show');
+    let msg_elm = document.getElementById('rock-spinner-modal-body');
+    if (msg_elm)
+      msg_elm.innerHTML = (msg ? msg : '');
+    self.modal({show:true, backdrop:'static'});
   }
 
   function hide() {
@@ -64,11 +82,26 @@ const Spinner = (function(){
 /**
 彈出訊息窗
 
-在使用者關閉訊息提示窗之前，調用 show() 要顯示的訊息內容，將會累加在提示窗中，而不是取代。
+在使用者關閉訊息提示窗之前，你調用的每個 show() 訊息內容將會累加在提示窗中，而不是取代。
 避免跳出太多訊息提示窗。
+
+方法
+----
 
 + MessageModal.show(msg)
 + MessageModal.hide()
++ MessageModal.title(content)
+
+自訂外觀
+--------
+
+MessageModal 會自行建立一個預設的 modal 外觀。但使用者也可以自訂。
+
+參考 https://getbootstrap.com/docs/4.0/components/modal/ 自訂 MessageModal 外觀。
+
++ MessageModal 要求一個 id 為 "rock-message-modal" 的 Bootstrap modal 。
++ modal 中必須包含一個 id 為 "rock-message-modal-body" 的區塊，用於顯示 msg 內容。
++ modal 可包含一個 id 為 "rock-message-modal-title" 的區塊，顯示視窗標題。
 
 Required: Bootstrap 4.1 or later
  */
@@ -78,7 +111,7 @@ const MessageModal = (function(){
   let hide_binding = false;
 
   function __init__() {
-    // 如果使用者沒有自己定義 id 為 rock-message-modal 的訊息區塊，就自己建一個。
+    // 如果使用者沒有定義 id 為 rock-message-modal 的訊息區塊，就自己建一個。
     if (!document.getElementById('rock-message-modal')) {
       // console.log('create modal div');
       let div = document.createElement('div');
@@ -135,9 +168,17 @@ const MessageModal = (function(){
     self.modal('hide');
   }
 
+  function title(content) {
+    __init__();
+    let title_elm = document.getElementById('rock-message-modal-title');
+    if (title_elm)
+      title_elm.innerHTML = content;
+  }
+
   return {
     show: show,
-    hide: hide
+    hide: hide,
+    title: title
   };
 })(); 
 // end MessageModal
