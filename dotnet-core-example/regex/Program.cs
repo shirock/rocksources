8d4929a66dec95c7db474dddb6366e84291a792f
rock <shirock@seed.net.tw>
2019-07-01 17:38:31 +0800

    .NET Core C# example

---
+++ dotnet-core-example/regex/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+/*
+.NET Core C# example
+Regex match and split.
+ */
+namespace Example
+{
+    class Program
+    {
+        static void IsMatch(string id)
+        {
+            var rs = @"^[a-zA-Z][\w]+$";
+            var regex = new Regex(rs);
+            var m = regex.IsMatch(id);
+            Console.WriteLine(String.Format("Is \"{0}\" match pattern {1}: {2}", id, rs, m));
+        }
+
+        static void Main(string[] args)
+        {
+            IsMatch("Rock123");
+            IsMatch("123Rock");
+            IsMatch("Rock 1@3");
+
+            var s = "card-012-345-x:67";
+            Regex pattern = new Regex(@"\d+");
+            MatchCollection matches = pattern.Matches(s);
+            Console.WriteLine($"符合數目: {matches.Count}");
+            if (matches.Count > 0) {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+        }                    
+    }
+}
---
+++ dotnet-core-example/string-format/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/*
+.NET Core C# example: 
+string format: fixed width number and padding with '0'.
+ */
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 123;
+            Console.WriteLine(String.Format("固定6位寬度數字，前置0:\n{0,6:D6}", n));
+            Console.WriteLine(String.Format("固定6位寬度數字，前置空白:\n{0,6}", n));
+
+            float fv = 12.34f;
+            Console.WriteLine(String.Format("固定3.1位寬度浮點數，前置0:\n{0,5:000.0}", fv));
+            Console.WriteLine(String.Format("固定3.1位寬度浮點數:\n{0,5:.0}", fv));
+        }
+    }
+}
