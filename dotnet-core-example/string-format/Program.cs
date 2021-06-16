e9a21b742dfebb248706628e9c9b40537052b1bf
rock <shirock.tw@gmail.com>
2021-06-16 21:25:00 +0800

    number as hex.

--- dotnet-core-example/string-format/Program.cs
+++ dotnet-core-example/string-format/Program.cs
@@ -13,6 +13,8 @@ namespace Example
             Console.WriteLine(String.Format("固定6位寬度數字，前置0:\n{0,6:D6}", n));
             Console.WriteLine(String.Format("固定6位寬度數字，前置空白:\n{0,6}", n));
 
+            Console.WriteLine(String.Format("16進位表示文字，前置0:\n{0:x2}, {1:x2}", n, 10));
+
             float fv = 12.34f;
             Console.WriteLine(String.Format("固定3.1位寬度浮點數，前置0:\n{0,5:000.0}", fv));
             Console.WriteLine(String.Format("固定3.1位寬度浮點數:\n{0,5:.0}", fv));
