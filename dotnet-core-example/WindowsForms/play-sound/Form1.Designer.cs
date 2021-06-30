c633ccb6bd103bb494fdaa40a545ebc6b20f4fbf
rock <shirock.tw@gmail.com>
2021-07-01 05:24:08 +0800

    how to play sound (Wav)

---
+++ dotnet-core-example/WindowsForms/play-sound/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(320, 240);
+            this.Text = "Form1";
+
+            this.Shown += new EventHandler(this.Form1_Shown);
+
+            this.Btn1 = new Button()
+            {
+                Location = new Point(10, 10),
+                AutoSize = true,
+                Text = "Play 1"
+            };
+            Btn1.Click += new EventHandler(this.BtnPlay_Click);
+            this.Controls.Add(Btn1);
+
+            this.Btn2 = new Button()
+            {
+                Location = new Point(10, 40),
+                AutoSize = true,
+                Text = "Play 2"
+            };
+            Btn2.Click += new EventHandler(this.BtnPlay_Click);
+            this.Controls.Add(Btn2);
+
+            this.Btn3 = new Button()
+            {
+                Location = new Point(10, 70),
+                AutoSize = true,
+                Text = "Play 3"
+            };
+            Btn3.Click += new EventHandler(this.BtnPlay_Click);
+            this.Controls.Add(Btn3);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ dotnet-core-example/WindowsForms/play-sound/Form1.cs
@@ -0,0 +1,47 @@
+﻿/*
+See also: https://docs.microsoft.com/zh-tw/dotnet/desktop/winforms/controls/how-to-play-a-sound-from-a-windows-form
+*/
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        public SoundPlayer[] players = new []
+            {
+                // Only support WAV file.
+                new SoundPlayer(@"c:\Windows\Media\chimes.wav"),
+                new SoundPlayer(@"c:\Windows\Media\Alarm01.wav"),
+                new SoundPlayer(@"c:\Windows\Media\Ring01.wav")
+            };
+
+        public Button Btn1;
+        public Button Btn2;
+        public Button Btn3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // SoundPlayer player1 = new SoundPlayer();
+            // player1.SoundLocation = @"c:\Windows\Media\chimes.wav";
+        }
+
+        private void Form1_Shown(object sender, System.EventArgs e)
+        {
+            players[0].Play();
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs s)
+        {
+            Button btn = (Button) sender;
+            
+            var i = Int32.Parse(btn.Text.Split(' ')[1]) - 1;
+            players[i].Play();
+        }
+    }
+}
