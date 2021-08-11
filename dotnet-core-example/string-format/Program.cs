821fe2d88a3d20f67b2609ee1e6e521f92c44c6b
rock <shirock.tw@gmail.com>
2021-08-11 15:53:00 +0800

    interpolation syntax

--- dotnet-core-example/string-format/Program.cs
+++ dotnet-core-example/string-format/Program.cs
@@ -10,14 +10,18 @@ namespace Example
         static void Main(string[] args)
         {
             int n = 123;
-            Console.WriteLine(String.Format("固定6位寬度數字，前置0:\n{0,6:D6}", n));
-            Console.WriteLine(String.Format("固定6位寬度數字，前置空白:\n{0,6}", n));
+            
+            // string.Format
+            Console.WriteLine(string.Format("固定6位寬度數字:\n前置0   :{0,6:D6}\n前置空白:{0,6}", n));
+            
+            // interpolation 
+            Console.WriteLine($"固定6位寬度數字:\n前置0   :{n,6:D6}\n前置空白:{n,6}");
 
             Console.WriteLine(String.Format("16進位表示文字，前置0:\n{0:x2}, {1:x2}", n, 10));
 
             float fv = 12.34f;
             Console.WriteLine(String.Format("固定3.1位寬度浮點數，前置0:\n{0,5:000.0}", fv));
-            Console.WriteLine(String.Format("固定3.1位寬度浮點數:\n{0,5:.0}", fv));
+            Console.WriteLine($"固定3.1位寬度浮點數:\n{fv,5:.0}");
         }
     }
 }
