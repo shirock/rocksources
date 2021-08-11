c4760fb912351b92932034922a85d18705504ef7
rock <shirock.tw@gmail.com>
2021-08-12 06:35:02 +0800

    process execute program

---
+++ dotnet-core-example/execute-external-program/exec-program/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Example
+{
+    class Program
+    {
+        static Process ExecProgram(string command)
+        {
+            string program;
+            string arg = "";
+            var pa = command.Split(' ', 2);
+            program = pa[0];
+            if (pa.Length > 1) 
+            {
+                arg = pa[1];
+            }
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = program,
+                    Arguments = arg,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            return process;
+        }
+
+        static void Main(string[] args)
+        {
+            var p1 = ExecProgram("xcopy /y Program.cs r:\\");
+            // var p1 = ExecCommand("cmd /C copy /y Program.cs r:\\");
+
+            // 非同步式
+            p1.Exited += (sender, ev) => {
+                System.Console.WriteLine("ok", p1.ExitCode);
+            };
+
+            // 同步式
+            p1.WaitForExit();
+            Console.WriteLine(p1.ExitCode);
+
+            ExecProgram("notepad");
+        }
+    }
+}
