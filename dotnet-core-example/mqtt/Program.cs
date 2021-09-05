fca202cdfb26acbbfa3bef159a7447780c0ae65e
rock <shirock.tw@gmail.com>
2021-09-05 10:03:56 +0800

    SimpleMqttClient

---
+++ dotnet-core-example/mqtt/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Mqtt;
+using System.Reactive.Linq;
+// 加上這個 Linq 套件， MessageStream 才能接 Where 方法。
+using System.Text;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var broker = "127.0.0.1";
+            var clientId = "jlfdalkfd";
+            var username = "test";
+            var password = "123";
+
+            var mqClient = new SimpleMqttClient(
+                    broker, 
+                    1883,
+                    username,
+                    password,
+                    clientId);
+
+            mqClient.Connect();
+            // 此處的 Main() 沒有宣告 async ，所以 connect 不用 await 的寫法。
+            // var sessionState = await mqClient.ConnectAsync();
+
+            mqClient.PublishAsync("tw/test", Encoding.UTF8.GetBytes("hello"));
+
+            mqClient.SubscribeAsync("tw/test/#");
+
+            // observer 設計模式
+            // Subscribe 可以串好幾個。
+            // 如果想要每個訂閱方法負責處理不同的主題，
+            // 就要在方法中加上主題判斷。
+            // 或是使用 System.Reactive.Linq ，
+            // 你就可以用 Where 。
+            mqClient.Client.MessageStream
+                .Subscribe(MessageArrived);
+
+            // MessageArrived1 只想處理 tw/test/1 的主題，
+            // 所以其方法開頭會判斷這個訊息的主題符不符合。
+            mqClient.Client.MessageStream
+                .Subscribe(MessageArrived1);
+
+            // 或者像下面這樣用 Where 。
+            // 只處理 topic 為 "tw/test/2" 的訊息。
+            mqClient.Client.MessageStream
+                .Where(msg => msg.Topic == "tw/test/2")
+                .Subscribe(msg => {
+                    Console.WriteLine(Encoding.UTF8.GetString(msg.Payload));
+                });
+
+            Console.WriteLine("press any key to end");
+            Console.Read();
+
+            mqClient.DisconnectAsync();
+            return;
+        }
+
+        public static void MessageArrived(MqttApplicationMessage msg)
+        {
+            var msg_text = Encoding.UTF8.GetString(msg.Payload);
+            Console.WriteLine(String.Format("topic {0}\n{1}", 
+                msg.Topic,
+                Encoding.UTF8.GetString(msg.Payload)));
+            return;
+        }
+
+        public static void MessageArrived1(MqttApplicationMessage msg)
+        {
+            if (msg.Topic != "tw/test/1")
+                return;
+
+            var msg_text = Encoding.UTF8.GetString(msg.Payload);
+            Console.WriteLine(String.Format("topic {0}\n{1}", 
+                msg.Topic,
+                Encoding.UTF8.GetString(msg.Payload)));
+            return;
+        }
+    }
+}
---
+++ dotnet-core-example/mqtt/SimpleMqttClient.cs
@@ -0,0 +1,111 @@
+﻿/*
+dotnet add package System.Net.Mqtt --version 0.6.3-beta
+
+See https://www.nuget.org/packages/System.Net.Mqtt
+Project site: https://github.com/xamarin/mqtt
+*/
+using System;
+using System.Net.Mqtt;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class SimpleMqttClient
+    {
+        public IMqttClient Client = null;
+        public MqttClientCredentials Credentials = null;
+
+        public SimpleMqttClient(string broker, int port)
+        {
+            var configuration = new MqttConfiguration {
+                Port = port
+            };
+            this.Client = MqttClient.CreateAsync(broker, configuration).Result;
+
+            this.Credentials = new MqttClientCredentials(clientId: GetAnonymousClientId());
+        }
+
+        public SimpleMqttClient(string broker, 
+                                int port, string 
+                                username, 
+                                string password, 
+                                string clientId=null) 
+            : this(broker, port)
+        {
+            // 若省略 clientId (null or "") 就隨機編一個
+            if (string.IsNullOrEmpty(clientId)) {
+                clientId = GetAnonymousClientId();
+            }
+            else {
+                var data = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(clientId));
+                // client id 基本上只用 a-zA-Z0-9 字元，用 MD5 將使用者輸入的 client id 轉成可用字元。
+                StringBuilder sbuf = new StringBuilder();
+                // MQTT 規格早期版本限制 client id 長度 23 字元。故從雜湊結果取22 HEX 字元(11位元組)。
+                for (var i = 0; i < 11; i++) {
+                    sbuf.Append(data[i].ToString("x2"));
+                }
+                clientId = sbuf.ToString();
+                // Console.WriteLine("The MD5 hash of " + clientId + " is: " + clientId + ".");
+            }
+
+            this.Credentials = new MqttClientCredentials(clientId, username, password);
+        }
+
+        internal static string GetAnonymousClientId() =>
+            string.Format(
+                "anonymous{0}",
+                Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 10)
+            );
+
+        public async Task PublishAsync(string topic, byte[] payload, int qos = 0)
+        {
+            var message = new MqttApplicationMessage(topic, payload);
+
+            await Client.PublishAsync(message, (MqttQualityOfService) qos);
+        }
+
+        /*
+        代表 qos 的列舉符號如下，但我習慣用 0, 1, 2。
+        MqttQualityOfService.AtMostOnce,
+        MqttQualityOfService.AtLeastOnce,
+        MqttQualityOfService.ExactlyOnce
+        */
+        public async Task SubscribeAsync(string topic, MqttQualityOfService qos = 0)
+        {
+            await Client.SubscribeAsync(topic, qos);
+        }
+
+        /*
+        System.Net.Mqtt 設計的服務連接方式 (ConnectAsync) 不太好用。
+        在連接 broker 之前，你不能訂閱主題。
+        所以你實際上得要把 ConnectAsync() 變成同步工作。
+
+        在非同步方法中呼叫 ConnectAsync() 時，寫法如下:
+        var sessionState = await mqClient.ConnectAsync();
+
+        在同步方法中呼叫 ConnectAsync() 時，寫法如下:
+        var sessionState = Client.ConnectAsync().Result;
+
+        所以我這裡直接設計成同步方法。
+
+        我不知道為何 System.Net.Mqtt (~v0.6.3) 有 Disconnected 事件，卻沒有 Connected 事件。
+        */
+        public SessionState Connect()
+        {
+             // Task.Run(()=>this.Client.ConnectAsync(Credentials)).Wait();
+            return Client.ConnectAsync(Credentials).Result;
+        }
+
+        public async Task<SessionState> ConnectAsync()
+        {
+            return await Client.ConnectAsync(Credentials);
+        }
+
+        public async Task DisconnectAsync()
+        {
+            await Client.DisconnectAsync();
+        }
+    } // end class
+}
