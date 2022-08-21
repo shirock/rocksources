3bd0b7f45db698706d32b1fd35ced1c8720ef730
rock <shirock.tw@gmail.com>
2022-08-22 06:05:49 +0800

    補充 ReaderWriterLockSlim 的使用說明
    1. ReaderWriterLockSlim 與 ReaderWriterLock  的差別
    2. 不適用多程序工作(multi-processing)

--- dotnet-core-example/file/file-write-lock/MyLog.cs
+++ dotnet-core-example/file/file-write-lock/MyLog.cs
@@ -5,7 +5,14 @@ using System.Threading;
 class MyLog
 {
     private static string logFile = "";
+    /*
+    ReaderWriterLockSlim 或 ReaderWriterLock 僅為同程序之多執行緒的資源共用鎖 (multi-threading only)。
+    不能用於不同程序之間的資源共用情形 (not for multi-processing)。
+    要在不同程序間共用資源，需要使用具名機制，例如 named system semaphore 或 named mutex.
+    https://docs.microsoft.com/en-us/dotnet/api/system.threading.semaphore
+    */
     private static ReaderWriterLockSlim fileLock = new ReaderWriterLockSlim();
+    // private static ReaderWriterLock fileLock = new ReaderWriterLock();
     // https://docs.microsoft.com/en-us/dotnet/api/system.threading.readerwriterlockslim
 
     public static void Setup(string filepath)
@@ -26,6 +33,7 @@ class MyLog
         // 當多緒同時開啟檔案時，後來者會擲出 IOException。
         // 若不想處理 IOException，則應使用 ReaderWriterLockSlim 保證不漏內容。
         fileLock.EnterWriteLock(); // 後來者將在此排隊等待
+        // fileLock.AcquireWriterLock(3000); // ReaderWriterLock
         try
         {
             File.AppendAllText(logFile, string.Format("{0} {1}\n", DateTime.Now.ToString("s"), msg));
@@ -38,6 +46,7 @@ class MyLog
         finally
         {
             fileLock.ExitWriteLock();
+            // fileLock.ReleaseWriterLock(); // ReaderWriterLock
         }
     }
 }
