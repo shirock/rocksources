bf8352eda13ddc2b0b3f0246bee30104ea127211
rock <shirock.tw@gmail.com>
2022-08-26 06:08:40 +0800

    增加文章使用的兩個範例程式 mqtt subscriber 和 publisher
    .NET MQTT 用戶端訂閱方法使用時的陷阱，關於 MaximumQualityOfService

---
+++ dotnet-core-example/mqtt-cases/subscribe-trap/publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿// .NET 6
+using System.Net.Mqtt;
+using System.Text;
+
+var broker = "localhost";
+var topic = "my/test";
+
+var configuration = new MqttConfiguration 
+{
+    Port = 1883,
+    MaximumQualityOfService = MqttQualityOfService.AtMostOnce
+};
+var mqClient = MqttClient.CreateAsync(broker, configuration).Result;
+_ = mqClient.ConnectAsync().Result;
+
+var count = 0;
+
+var pubTimer = new Timer(state => {
+    var msg = new MqttApplicationMessage(
+        topic, 
+        Encoding.UTF8.GetBytes(count.ToString()));
+
+    mqClient.PublishAsync(msg, MqttQualityOfService.AtLeastOnce);
+    // 發佈訊息指定的 qos 大於 AtMostOnce (0)
+
+    Console.WriteLine(string.Format("{0} publish {1}"), 
+        DateTime.Now.ToString("s"), count);
+    count++;
+}, null, 500, 20000);
+
+Console.WriteLine("press Enter to end.");
+Console.ReadLine();
---
+++ dotnet-core-example/mqtt-cases/subscribe-trap/subscriber/Program.cs
@@ -0,0 +1,30 @@
+﻿// .NET 6
+using System.Net.Mqtt;
+using System.Text;
+
+var broker = "localhost";
+var topic = "my/test";
+
+var configuration = new MqttConfiguration 
+{
+    Port = 1883,
+    MaximumQualityOfService = MqttQualityOfService.ExactlyOnce
+    // MaximumQualityOfService = MqttQualityOfService.AtMostOnce
+    // 預設是 AtMostOnce 。
+    // 如果 MaximumQualityOfService 和訂閱時的 qos 不匹配，
+    // 則訂閱者最多可收到20次訊息。超過就會失聯。
+    // 我也不知道為什麼定這個數目。也不知道為何不擲出例外。
+};
+var mqClient = MqttClient.CreateAsync(broker, configuration).Result;
+_ = mqClient.ConnectAsync().Result;
+
+mqClient.SubscribeAsync(topic, MqttQualityOfService.ExactlyOnce);
+
+mqClient.MessageStream.Subscribe(msg => {
+    var msgText = Encoding.UTF8.GetString(msg.Payload);
+    var timeStamp = DateTime.Now.ToString("s");
+    Console.WriteLine($"{timeStamp} receive {msgText}");
+});
+
+Console.WriteLine("press Enter to end.");
+Console.ReadLine();
