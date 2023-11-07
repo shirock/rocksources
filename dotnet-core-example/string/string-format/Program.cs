34f0060030fa815a0c124572c1c1db3a5ab905ed
rock <shirock.tw@gmail.com>
2023-11-08 01:01:21 +0800

    move to string folder.

--- dotnet-core-example/string/string-format/Program.cs
+++ dotnet-core-example/string/string-format/Program.cs
@@ -17,10 +17,10 @@ namespace Example
             // interpolation 
             Console.WriteLine($"固定6位寬度數字:\n前置0   :{n,6:D6}\n前置空白:{n,6}");
 
-            Console.WriteLine(String.Format("16進位表示文字，前置0:\n{0:x2}, {1:x2}", n, 10));
+            Console.WriteLine(string.Format("16進位表示文字，前置0:\n{0:x2}, {1:x2}", n, 10));
 
             float fv = 12.34f;
-            Console.WriteLine(String.Format("固定3.1位寬度浮點數，前置0:\n{0,5:000.0}", fv));
+            Console.WriteLine(string.Format("固定3.1位寬度浮點數，前置0:\n{0,5:000.0}", fv));
             Console.WriteLine($"固定3.1位寬度浮點數:\n{fv,5:.0}");
         }
     }
