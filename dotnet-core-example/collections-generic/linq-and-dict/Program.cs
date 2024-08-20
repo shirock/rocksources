13c7fc885582a67dff59b8018081da8c8582fa63
rock <shirock.tw@gmail.com>
2024-08-20 18:51:32 +0800

    dotnet use colletions with linq.

---
+++ dotnet-core-example/collections-generic/linq-and-dict/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+將 Dictionary 當作有主鍵(primary key)的資料表。
+並求集合:
+1. SELECT * FROM items WHERE code > 1;
+2. SELECT key FROM items WHERE key NOT IN selected;
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Item
+{
+    public string name;
+    public int code;
+
+    public Item(string n, int c)
+    {
+        name = n;
+        code = c;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var items = new Dictionary<string, Item>
+        {
+            {"a", new Item("nameA", 1)},
+            {"b", new Item("nameB", 2)},
+        };
+        // Add method
+        items.Add("c", new Item("nameC", 3));
+
+        // SELECT * FROM items WHERE code > 1;
+        var gt1 = from item in items
+                    where item.Value.code > 1
+                    select item;
+
+        // 函式敘述
+        var gt2 = items.
+            Where(items => items.Value.code > 1).
+            Select(item => item);
+
+        Console.WriteLine("Greater Than 1:");
+        foreach (var item in gt1)
+            Console.WriteLine($"{item.Key}: {item.Value.name}|{item.Value.code}");
+
+        var selected = new HashSet<string>
+        {
+            "a",
+            "c"
+        };
+
+        // SELECT key FROM items WHERE key NOT IN selected;
+        var unselected =
+            from key in items.Keys
+                where !selected.Contains(key)
+                select key;
+
+        // 函式敘述
+        var unselected2 = items.Keys.
+            Where(key => !selected.Contains(key)).
+            Select(key => key);
+
+        Console.WriteLine("Unselected:");
+        foreach (var key in unselected)
+            Console.WriteLine(key);
+    }
+}
---
+++ dotnet-core-example/collections-generic/linq-group-sum/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+分組求和。
+SELECT name, SUM(qty) FROM items GROUP BY name;
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Item
+{
+    public string Name;
+    public int Qty;
+
+    public Item(string n, int c)
+    {
+        Name = n;
+        Qty =  c;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var items = new List<Item>
+        {
+            new Item("a", 1),
+            new Item("b", 2),
+            new Item("c", 3),
+        };
+        // Add method
+        items.Add(new Item("a", 4));
+
+        // SELECT name, SUM(qty) FROM items GROUP BY name;
+        var gsum = 
+            from item in items
+            group item by item.Name into g
+            select new
+            {
+                Name = g.Key,
+                TotalQty = g.Sum(item => item.Qty)
+            };
+
+        // 函式敘述
+        var gs2 = items.
+            GroupBy(item => item.Name).
+            Select(g => new
+            {
+                Name = g.Key,
+                TotalQty = g.Sum(item => item.Qty)
+            });
+
+        foreach (var name_qty in gsum)
+            Console.WriteLine($"{name_qty.Name} total {name_qty.TotalQty}");
+    }
+}
